bf1ac08a99277d106eb8df79d56942fd5465e572
Jullan118 <julia.blombergg@gmail.com>
2026-09-23 16:00:08 +0200

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -1,6 +1,10 @@
-import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
-import { HamburgerMenu } from "@/components/HamburgerMenu";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { MainNav } from "@/components/MainNav";
 import { MiniGlobe } from "@/components/MiniGlobe";
+import { CartCorner } from "@/components/CartCorner";
+import { SiteFooter } from "@/components/SiteFooter";
+import { CartDrawer } from "@/components/CartDrawer";
+import { CartProvider } from "@/lib/cart";
 import { Home } from "@/pages/Home";
 import { NewFlames } from "@/pages/NewFlames";
 import { NewFlamesProduct } from "@/pages/NewFlamesProduct";
@@ -11,50 +15,14 @@ import { Trash } from "@/pages/Trash";
 import { TrashProduct } from "@/pages/TrashProduct";
 import { TrashUpload } from "@/pages/TrashUpload";
 
-function SiteFooter() {
-  const { pathname } = useLocation();
-  if (pathname !== "/about") return null;
-
-  return (
-    <footer className="absolute bottom-1 w-full px-6 font-skarp text-black text-xl sm:text-2xl leading-none">
-      <div className="max-w-[28rem] mx-auto grid grid-cols-2 gap-y-0 gap-x-4 text-center leading-none">
-        <span className="sm:text-left sm:pl-3">Stuck in stuckholm</span>
-        <span className="sm:text-right">all rights reserved {new Date().getFullYear()}</span>
-
-        <a
-          href="mailto:hello.stuckholm@gmail.com"
-          className="sm:text-left sm:pl-3 hover:opacity-70 transition-opacity"
-        >
-          hello.stuckholm@gmail.com
-        </a>
-        <span className="sm:text-right">2026, STOCKHOLM</span>
-
-        <a
-          href="https://instagram.com/stuckholm.se"
-          target="_blank"
-          rel="noreferrer"
-          className="sm:text-left sm:pl-3 hover:opacity-70 transition-opacity"
-        >
-          INSTAGRAM
-        </a>
-        <a href="tel:+46011771123" className="sm:text-right hover:opacity-70 transition-opacity">
-          +46 011 77 11 23
-        </a>
-      </div>
-
-      <div className="max-w-2xl mx-auto text-center mt-0">
-        <span>By aquam3ss</span>
-      </div>
-    </footer>
-  );
-}
-
 function App() {
   return (
     <BrowserRouter>
+      <CartProvider>
       <main className="min-h-screen w-full bg-[#ffffff] flex flex-col items-center relative">
         <MiniGlobe />
-        <HamburgerMenu />
+        <MainNav />
+        <CartCorner />
 
         <Routes>
           <Route path="/" element={<Home />} />
@@ -72,6 +40,8 @@ function App() {
 
         <SiteFooter />
       </main>
+      <CartDrawer />
+      </CartProvider>
     </BrowserRouter>
   );
 }
